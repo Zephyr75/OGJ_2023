2a878fb90ad660513203bb0f40686b57f9b62842
Antoine <antoine.familleroger@gmail.com>
2023-03-19 10:15:35 +0100

    Added dialog

--- Assets/Scripts/CopyPlayer.cs
+++ Assets/Scripts/CopyPlayer.cs
@@ -56,7 +56,7 @@ public class CopyPlayer : MonoBehaviour
 
     IEnumerator Activate()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(15.0f);
         isActive = true;
     }
 }
--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Intro : MonoBehaviour
 {
+    [SerializeField]
+    TextMeshProUGUI text;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +25,18 @@ public class Intro : MonoBehaviour
         yield return new WaitForSeconds(0f);//TODO replace by 6f
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
         GameObject.Find("Player").GetComponent<Rigidbody>().isKinematic = false;
+        List<string> texts = new List<string>();
+        texts.Add("You will not escape alive, subject 2077...");
+        texts.Add("I am in your mind, subject 2077...");
+        texts.Add("No need to run, subject 2077...");
+        text.text = "";
+        foreach (string line in texts) {
+            foreach (char letter in line.ToCharArray()) {
+                text.text += letter;
+                yield return new WaitForSeconds(0.1f);
+            }
+            yield return new WaitForSeconds(2f);
+            text.text = "";
+        }
     }
 }
