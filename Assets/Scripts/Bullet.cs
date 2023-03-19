c72f3db24aabad34da4f9a1603ac8474739d7d6a
Antoine <antoine.familleroger@gmail.com>
2023-03-19 01:06:57 +0100

    Added lasers

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float lifeTime = 3f;
+    private float speed = 20f;
+    private float maxLifeTime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        lifeTime += Time.deltaTime;
+        if (lifeTime >= maxLifeTime) {
+            Destroy(gameObject);
+        }
+
+
+    }
+
+
+}
